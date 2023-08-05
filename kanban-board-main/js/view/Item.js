f709ea63bb2e3f149b0706cf9625b10b4070254f
jakeolase <mail.jakeolase@gmail.com>
2023-08-05 18:49:17 +0800

    Transferring Flowv1.0 right click function in Items.js

--- kanban-board-main/js/view/Item.js
+++ kanban-board-main/js/view/Item.js
@@ -4,6 +4,10 @@ import KanbanAPI from "../api/KanbanAPI.js";
 export default class Item {
 	constructor(id, content) {
 		const bottomDropZone = DropZone.createDropZone();
+		const customContextMenu = document.querySelector(".custom-context-menu");
+
+		this.rclicked = false;
+
 
 		this.elements = {};
 		this.elements.root = Item.createRoot();
@@ -34,15 +38,72 @@ export default class Item {
 		};
 
 		this.elements.input.addEventListener("blur", onBlur);
-		this.elements.root.addEventListener("dblclick", () => {
-			const check = confirm("Are you sure you want to delete this item?");
+		// this.elements.root.addEventListener("dblclick", () => {
+		// 	const check = confirm("Are you sure you want to delete this item?");
 
-			if (check) {
-				KanbanAPI.deleteItem(id);
+		// 	if (check) {
+		// 		KanbanAPI.deleteItem(id);
 
-				this.elements.input.removeEventListener("blur", onBlur);
-				this.elements.root.parentElement.removeChild(this.elements.root);
-			}
+		// 		this.elements.input.removeEventListener("blur", onBlur);
+		// 		this.elements.root.parentElement.removeChild(this.elements.root);
+		// 	}
+		// });
+
+		//event listeners for right click menu
+		this.elements.root.addEventListener("contextmenu", (e) => {
+			e.preventDefault();
+
+			this.rclicked = true;
+			let topPosition = e.clientY;
+			let leftPosition = e.clientX;
+			customContextMenu.classList.add("active");
+
+			customContextMenu.style.left = leftPosition + "px";
+			customContextMenu.style.top = topPosition + "px";
+
+			const menu = document.getElementById("menu");
+			this.elements.root.style.borderColor = "#0f67aacc";
+			this.elements.root.style.borderWidth = "2px";
+
+			//event listener for right click menu. 
+			menu.addEventListener("click", (event) => {
+
+				const clickedElement = event.target;
+
+				if (clickedElement.id === "bold") {
+					boldText(id, element)
+					this.elements.root.style.borderColor = "#d4d4d4";
+					this.elements.root.style.borderWidth = "1px";
+				}
+
+				if (clickedElement.id === "del") {
+					if (this.rclicked) {
+						KanbanAPI.deleteItem(id);
+						this.elements.input.removeEventListener("blur", onBlur);
+						this.elements.root.parentElement.removeChild(this.elements.root);
+					}
+				}
+				if (clickedElement.id == "highlight") {
+					highlightText(id, element);
+					divElement.style.borderColor = "#d4d4d4";
+					divElement.style.borderWidth = "1px";
+				}
+				if (clickedElement.id === "ital") {
+					italicText(id, element);
+					divElement.style.borderColor = "#d4d4d4";
+					divElement.style.borderWidth = "1px";
+				}
+
+				this.rclicked = false;
+			}, { once: true });
+		});
+
+
+		window.addEventListener("click", () => {
+			this.rclicked = false;
+			customContextMenu.classList.remove("active");
+			this.elements.root.style.borderColor = "#d4d4d4";
+			this.elements.root.style.borderWidth = "1px";
 		});
 
 		this.elements.root.addEventListener("dragstart", e => {
@@ -51,6 +112,10 @@ export default class Item {
 
 		this.elements.input.addEventListener("drop", e => {
 			e.preventDefault();
+		});
+
+		this.elements.root.addEventListener("contextmenu", () => {
+			this.isRightClicked = false;
 		});
 	}
 
