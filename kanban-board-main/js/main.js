a02be8064c6654b36f8b962e06360003eb28cd66
jakeolase <mail.jakeolase@gmail.com>
2023-12-26 23:26:40 +0800

    Added DateTime functionality on sidebar and removed button group

--- kanban-board-main/js/main.js
+++ kanban-board-main/js/main.js
@@ -3,3 +3,25 @@ import Kanban from "./view/Kanban.js";
 new Kanban(
 	document.querySelector(".kanban")
 );
+
+function updateDateTime() {
+	var currentDate = new Date();
+	var options = { 
+		month: 'long', 
+		day: 'numeric', 
+		year: 'numeric', 
+		hour: 'numeric', 
+		minute: 'numeric', 
+		hour12: true 
+	};
+
+	var formattedDateTime = currentDate.toLocaleString('en-US', options);
+
+	document.getElementById('datetime').innerText = formattedDateTime;
+}
+
+// Initial update
+updateDateTime();
+
+// Update every second (1000 milliseconds)
+setInterval(updateDateTime, 1000);
